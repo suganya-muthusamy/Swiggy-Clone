d0d0821030ed05da49b8bab4d65f0a7e024a86c0
suganya-muthusamy <suganyamuthu9560@gmail.com>
2023-04-17 02:18:38 -0600

    Episode 02 - Igniting the app

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-
-/**
- * how to create nested elements using react
- * <div id="parent">
-    * <div id="child">
-    *      <h1>Hello World from nested React!</h1>
-    * </div>
- * </div>
- */
+import React from "react"
+import ReactDOM from "react-dom/client"
 
 const headTag = React.createElement("div", {id:"parent"}, 
                 [React.createElement("div", {id:"child1"}, 
-                [React.createElement("h1", { class:"headClass"}, "Hello World from nested React 00:h1!"),
-                React.createElement("h2", { class:"headClass"}, "Hello World from nested React 00:h2!")]),
+                [React.createElement("h1", { className:"headClass"}, "Hello World from nested React 00:h1!"),
+                React.createElement("h2", { className:"headClass"}, "Hello World from nested React 00:h2!")]),
                 React.createElement("div", {id:"child2"}, 
-                [React.createElement("h1", { class:"headClass"}, "Hello World from nested React 01:h1!"),
-                React.createElement("h2", { class:"headClass"}, "Hello World from nested React 01:h2!")])])
+                [React.createElement("h1", { className:"headClass"}, "Hello World from nested React 01:h1!"),
+                React.createElement("h2", { className:"headClass"}, "Hello World from nested React 01:h2!")])])
 
 // const headTag=React.createElement('h1', {
 //     id:"headID", className:"headClass"
