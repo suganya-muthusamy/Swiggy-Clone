29286d0c1a6570f211bb60cf61ed1a53ad65aae0
suganya-muthusamy <suganyamuthu9560@gmail.com>
2023-04-25 03:51:45 -0600

    adding logo, search bar, profile image and wrote CSS

--- App.js
+++ App.js
@@ -1,35 +1,23 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-
-// react element
-const headTag = (
-<h1 className="headClass">Hello from JSX Element</h1>
-)
-
-// react component
-const Title = () => (
-    <h1 className="headClass">Vanakkam React</h1>
-)
-
-// variable
-const text="Functional component";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 // functional component
-const Head = () => {
-    return(
-        <div>
-            <h1>Hello from Head component</h1>
-        </div>
-    )
-}
+const Header = () => {
+  return (
+    <div className="header-container">
+      <img
+        alt="logo"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2WazG9ogzCX-edCo-gI8grlP6RMHwR7otmg&usqp=CAU"
+      />
+      <div className="search-container">
+        <input type="text" /> 
+        <button type="submit">search</button>
+      </div>
+      <img src="https://scontent.fyxd1-1.fna.fbcdn.net/v/t1.6435-9/107004184_110965414017381_1184064122406866696_n.jpg?stp=cp0_dst-jpg_e15_p320x320_q65&_nc_cat=103&ccb=1-7&_nc_sid=7aed08&_nc_ohc=FzS7Zs1eSqgAX8PdD8C&_nc_ht=scontent.fyxd1-1.fna&oh=00_AfBEme4Wschlc1jWjDY4eoeTdgCK5K2z0KiBQacCfkpIbg&oe=646F1049" />
+    </div>
+  );
+};
 
-const root=ReactDOM.createRoot(document.getElementById('root'))
-const root1=ReactDOM.createRoot(document.getElementById('root1'))
-const root2=ReactDOM.createRoot(document.getElementById('root2'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-
-root.render(headTag)
-root1.render(<Title/>)
-root2.render(text)
-
-
+root.render(<Header />);
